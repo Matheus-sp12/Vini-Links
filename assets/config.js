8657c9bc7b93477cad417571245f6e1871bbb7a9
matheus <viniciusomagnifico@hotmail.com>
2025-02-10 00:50:15 -0300

    first commit

---
+++ assets/config.js
@@ -0,0 +1,32 @@
+let config = {
+  "Title": "Meus Links",
+  "Name": "Software Engineer",
+  "Description": "NextJS | Typescript | NodeJS | ReactJS",
+  "links": [
+    {
+      "Title": "Portifolio",
+      "URL": "https://portifolio2-0-virid.vercel.app/",
+      "icon_classes": "fas fa-link",
+    },
+    {
+      "Title": "linkedin",
+      "URL": "https://www.linkedin.com/in/vin%C3%ADcius-silva-450b771a0/",
+      "icon_classes": "fab fa-linkedin",
+    },
+    {
+      "Title": "Instagram",
+      "URL": "https://www.instagram.com/vinicius_math15/",
+      "icon_classes": "fab fa-instagram"
+    },
+    {
+      "Title": "WhatsApp",
+      "URL": "https://wa.me/5511986427140",
+      "icon_classes": "fab fa-whatsapp"
+    },
+    {
+      "Title": "Email",
+      "URL": "mailto:viniciussilva_1@hotmail.com",
+      "icon_classes": "fa fa-envelope"
+    },
+  ]
+}
